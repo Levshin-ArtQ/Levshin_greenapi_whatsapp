81c1e9883c4e6cb91568fad6bfb2819c5c09d022
Levshin-ArtQ <114755543+Levshin-ArtQ@users.noreply.github.com>
2025-02-11 10:38:08 +0500

    chat history fix

--- src/api/greenApi.js
+++ src/api/greenApi.js
@@ -33,20 +33,9 @@ export const deleteNotification = async (idInstance, apiTokenInstance, receiptId
 };
 
 export const getChatHistory = async (idInstance, apiTokenInstance, chatId) => {
-  let data = JSON.stringify({
-    "chatId": "79084947899@с.us",
-    "count": 12
-  });
-  let config = {
-    method: 'get',
-    maxBodyLength: Infinity,
-    url: 'https://1103.api.green-api.com/waInstance1103187535/getChatHistory/f34ecd95165f4bd7adb1f970815b40ef951a609f25504b1b9e',
-    headers: { 
-      'Content-Type': 'application/json'
-    },
-    data : data
-  };
-  const response = await axios.request(config);
-  console.log('get chat history', response);  
+  const response = await axios.get(
+    `${BASE_URL}/waInstance${idInstance}/GetChatHistory/${apiTokenInstance}/${chatId}`
+  );
+  console.log('get chat history', response.data);
   return response.data;
 }
